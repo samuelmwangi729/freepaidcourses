20c085cd370e6dd3ce2ab1d24986d6d19679d7af
samuel mwangi <samuelmwangi729@gmail.com>
2020-06-21 11:54:02 +0300

    final

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -192,6 +192,17 @@
               </li>
             </ul>
           </li>
+          <li>
+            <a class="dropdown-item" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+                          document.getElementById('logout-form').submit();">
+             {{ __('Logout') }}
+         </a>
+
+         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+             @csrf
+         </form>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
